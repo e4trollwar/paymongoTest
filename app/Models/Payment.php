7542a5d987ddf62fa62670ba7a5c1b29a21d7fc9
unknown <angeloarimas@gmail.com>
2024-05-22 15:18:47 +0800

    asdasd

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+    protected $table = 'payments';
     protected $fillable = ['user_id','stripe_id','subtotal','tax','total'];
     use HasFactory;
 }
